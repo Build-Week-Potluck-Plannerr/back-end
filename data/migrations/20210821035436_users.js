74a5798b75085c1676473cc9f4414bbbc3d95c2a
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-08-26 00:28:36 -0700

    :sparkles: tables
    new food table coming, new event table coming

--- data/migrations/20210821035436_users.js
+++ data/migrations/20210821035436_users.js
@@ -1,12 +1,28 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', (table) => {
-    table.increments();
-    table.text('username').unique().notNullable();
-    table.text('password').notNullable();
-    table.text('name').notNullable();
-    table.text('role').defaultTo('guest');
-  });
+  return knex.schema
+      .createTable('users', (table) => {
+        table.increments();
+        table.text('username').unique().notNullable();
+        table.text('password').notNullable();
+        table.text('name').notNullable();
+        table.text('role').defaultTo('guest');
+      })
+      .createTable('potluck dishes', (table) => {
+        table.increments();
+        table.text('drinks');
+        table.text('entree');
+        table.text('appetizers');
+        table.text('sides');
+        table.text('dessert');
+      })
+      .createTable('potluck event', (table)=> {
+        table.increments();
+        table.text('name');
+        table.text('date');
+        table.text('time');
+        table.text('location');
+      });
 };
 
 exports.down = function(knex, Promise) {
